8da9281b836330d34c094179242125f5596d9796
Lance Petersen <lance@goskip.com>
2020-02-21 22:05:54 -0700

    Insert into db

--- chatty/views.py
+++ chatty/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 import boto3
+import uuid
 
 dynamodb = boto3.resource('dynamodb')
 
 def hello(request):
     """My first view"""
     table = dynamodb.Table('Chat')
+
     print(table.creation_date_time)
-    return render(request, 'stuff/hello.html', {'name': 'ellie'})
+
+    table.put_item(
+        Item={
+            'chatId': 'general',
+            'chatGroupId': 'first',
+            'message': 'Hello World 2!'
+        }
+    )
+    return render(request, 'stuff/hello.html', {'name': 'different thing'})
